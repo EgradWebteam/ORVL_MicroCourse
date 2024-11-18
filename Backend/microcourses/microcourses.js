a070365db4a2491263b476c70262b8581bb715c9
nandini <nandini.u@egradtutor.in>
2024-11-18 17:19:06 +0530

    newcommit

--- Backend/microcourses/microcourses.js
+++ Backend/microcourses/microcourses.js
@@ -124,45 +124,7 @@ router.get('/my_courses/:userId', async (req, res) => {
         res.status(500).send('Internal Server Error');
     }
 });
-router.get('/user-profile/:userId', async (req, res) => {
-    const userId = req.params.userId; // Get userId from URL parameters
 
-    try {
-        // SQL query to fetch user profile data
-        const query = `
-            SELECT 
-                r.Candidate_Name, 
-                r.Email_Id, 
-                r.Contact_Number, 
-                r.Candidate_Photo
-            FROM 
-                registration_details r
-            JOIN 
-                login_details l 
-            ON 
-                r.student_registration_Id = l.student_registration_Id
-            WHERE 
-                l.user_Id = ?;
-        `;
-
-        // Execute the query with userId
-        const [rows] = await db.query(query, [userId]);
-
-        if (rows.length > 0) {
-            const userProfile = rows[0];
-            // If the user has a profile picture, encode it in base64
-            if (userProfile.Candidate_Photo) {
-                userProfile.Candidate_Photo = Buffer.from(userProfile.Candidate_Photo).toString('base64');
-            }
-            res.json(userProfile);
-        } else {
-            res.status(404).json({ message: 'User profile not found' });
-        }
-    } catch (error) {
-        console.error('Error fetching user profile:', error);
-        res.status(500).send('Internal Server Error');
-    }
-});
 
 router.get('/my_final_tests/:userId', async (req, res) => {
     const userId = req.params.userId;
@@ -766,7 +728,7 @@ router.get('/accessStatus/:userId/:courseCreationId', async (req, res) => {
         );
         const vv = videoVisits.length; 
         const visitedVideoIds = videoVisits.map(row => row.unit_Id);
-        const visitedVideos = videos.map(video => video.unit_Id);
+        const visitedVideos = videoVisits.map(video => video.unit_Id);
         const videoCount = videoVisits.map(video => ({
             unit: video.unit_Id, 
             videoCount: video.video_count
